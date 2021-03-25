3d9dbbfd9eaa4e6b900e08199742dd6a1082a802
SeifAlmoatazBellah <seifalmoataz@protonmail.com>
2021-03-25 21:19:22 +0200

    Start coding server.

---
+++ server/main.py
@@ -0,0 +1,15 @@
+from fastapi import Depends, FastAPI
+
+# from .dependencies import get_query_token, get_token_header
+# from .internal import admin
+from routers import users
+
+app = FastAPI()
+
+
+app.include_router(users.router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Bigger Applications!"}
