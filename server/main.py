68fe37649850628c2db63da3b41e8e2202056dfb
SeifAlmoatazBellah <seifalmoataz@protonmail.com>
2021-03-25 21:29:01 +0200

    add pymongo

--- server/main.py
+++ server/main.py
@@ -1,14 +1,8 @@
 from fastapi import Depends, FastAPI
-
-# from .dependencies import get_query_token, get_token_header
-# from .internal import admin
 from routers import users
 
 app = FastAPI()
-
-
 app.include_router(users.router)
-
 
 @app.get("/")
 async def root():
